12f4f5d36b2170ada4b0c356dbf9ada081bd0047
fabian-ss <randommusicd@gmail.com>
2023-03-26 19:17:02 -0600

    TailwindCSS Added

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import {Provider} from 'react-redux';
 import { store } from './store';
+import HomeScreenNotes from './src/Notes/HomeScreenNotes';
+
+
 
 const App = () => {
   return (
     <Provider store={store}>
-      <View>
-      <Text>App</Text>
-      </View>
+      <HomeScreenNotes/>
     </Provider>
   )
 }
---
+++ src/commands.js
@@ -0,0 +1,5 @@
+{
+    Install:[
+        "yarn add tailwind-react-native-classnames"
+    ]
+}
